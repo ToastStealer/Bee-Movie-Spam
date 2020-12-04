4a1bf9aee6773aed08e7268e1665e72738277d6e
ToastStealer <73896209+ToastStealer@users.noreply.github.com>
2020-12-04 15:57:37 +0000

    Delete Software.py

--- Software.py
+++
@@ -1,9 +0,0 @@
-import time
-from pynput.keyboard import Key, Controller
-text = open("text.txt", "r")
-keyboard = Controller()
-print("Program starting in 7 secs...")
-time.sleep(7)
-for x in text:
-    keyboard.type(x)
-    time.sleep(5)
